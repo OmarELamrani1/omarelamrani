883024de9f054c7722f627d0b0f26b5490284033
OmarELamrani1 <omar.elcopa@gmail.com>
2022-06-01 18:31:29 +0100

    website

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,20 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/main.css'],
+    'bulma',
+  css: [
+    '@/assets/css/main.css',
+    '@/assets/css/main.scss'
+  ],
+
+  css: [
+    // Load a Node.js module directly (here it's a Sass file)
+    'bulma',
+    // CSS file in the project
+    '~/assets/css/main.css',
+    // SCSS file in the project
+    '~/assets/css/main.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
